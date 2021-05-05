6fba4bf18252b9c087a856aa810f2db45c41a859
gauransh <gauranshk21@gmail.com>
2021-05-05 23:36:59 +0530

    Added Donor Reciver Screen and fixed the images in screens

--- lib/constants/image_constants.dart
+++ lib/constants/image_constants.dart
@@ -1,5 +1,7 @@
-class ImageConstants{
-  static const String LOGO_IMAGE_URL = "assets/images/logo.png";
-  static const String LOGIN_SCREEN_IMAGE_URL = "assets/images/image_1.png";
-  static const String OTP_SCREEN_IMAGE_URL = "assets/images/image_2.png";
+class ImageConstants {
+  static const String LOGO_IMAGE_URL = "assets/images/logo.svg";
+  static const String LOGIN_SCREEN_IMAGE_URL = "assets/images/image_1.svg";
+  static const String OTP_SCREEN_IMAGE_URL = "assets/images/image_2.svg";
+  static const String DONOR_RECIEVER_SCREEN_IMAGE_URL =
+      "assets/images/image_3.svg";
 }
